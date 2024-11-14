62c3f379cff70bb5eefee90389a45f21e1a49a49
Lukasz Kwiatkowski <lkwiatkowski@outlook.com>
2024-11-14 00:09:28 +0000

    cross_get added to tests

--- test.py
+++ test.py
@@ -7,12 +7,21 @@ if os.path.exists("test.db"):
     os.remove("test.db")
 
 @dataclass
+class ExampleCrossTable:
+    id: int = Field(FieldType.INTEGER, True, True)
+    cross_value: float = Field(FieldType.REAL)
+
+@dataclass
 class ExampleTable:
     id: int = Field(FieldType.INTEGER, True, True)
     name: str = Field(FieldType.TEXT)
     value: float = Field(FieldType.REAL)
 
-orm = Engine("test", [ExampleTable], True)
+orm = Engine("test", [ExampleCrossTable, ExampleTable], True)
+
+cross_table = orm.get_table("ExampleCrossTable")
+cross_table.add(ExampleCrossTable(None, 20.0))
+cross_table.add(ExampleCrossTable(None, 30.0))
 
 table = orm.get_table("ExampleTable")
 
@@ -22,39 +31,46 @@ table.add(ExampleTable(None,"NAME3",1.0))
 orm.commit()
 
 for e in table.get_all():
-    print(e.__dict__)
+    print(e)
 
 class TestTableMethods(unittest.TestCase):
     def test01_get_all(self):
-        res: list[ExampleTable] = table.get_all()
+        res = table.get_all()
         self.assertEqual(len(res), 3)
-        self.assertEqual(res[0].id, 1)
-        self.assertEqual(res[1].id, 2)
-        self.assertEqual(res[2].id, 3)
+        self.assertEqual(res[0][0], 1)
+        self.assertEqual(res[1][0], 2)
+        self.assertEqual(res[2][0], 3)
 
     def test02_get(self):
         wb = WhereBuilder("name", "NAME2")
-        res: list[ExampleTable] = table.get(wb)
+        res = table.get(wb)
         self.assertEqual(len(res), 1)
-        self.assertEqual(res[0].id, 2)
+        self.assertEqual(res[0][0], 2)
         wb = WhereBuilder("name", "NAME1").OR("name", "NAME3")
-        res: list[ExampleTable] = table.get(wb)
+        res = table.get(wb)
         self.assertEqual(len(res), 2)
-        self.assertEqual(res[0].id, 1)
-        self.assertEqual(res[1].id, 3)
+        self.assertEqual(res[0][0], 1)
+        self.assertEqual(res[1][0], 3)
 
     def test03_update(self):
         wb = WhereBuilder("name", "NAME2")
         new = ExampleTable(None, None, 4.0)
         table.update(wb, new)
-        res: list[ExampleTable] = table.get(wb)
-        self.assertEqual(res[0].value, 4.0)
+        res = table.get(wb)
+        self.assertEqual(res[0][2], 4.0)
 
     def test04_remove(self):
         wb = WhereBuilder("name", "NAME2")
         table.remove(wb)
-        res: list[ExampleTable] = table.get_all()
-        self.assertEqual(res[1].id, 3)
+        res = table.get_all()
+        self.assertEqual(res[1][0], 3)
+    
+    def test05_cross_get(self):
+        wb = WhereBuilder("name", "NAME1")
+        cross = WhereBuilder("id", "id")
+        res = table.cross_get("ExampleCrossTable", wb, cross)
+        print(res)
+        self.assertEqual(len(res), 1)
 
 if __name__ == '__main__':
     unittest.main()
